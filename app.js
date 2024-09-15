a52cf907dd5257fbfb7d2691aedecee2aad19b5d
Yuannruiii <yuanruicao998@gmail.com>
2024-09-15 22:36:03 +1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+const port = 4000;
+const path = require('path');
+
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '2002812918',
+	database: 'crowdfunding_db'
+});
+
+connection.connect(err =>{
+	if (err) {
+		console.error('Error connecting to the database:' + err.stack);
+		return;
+	}
+	console.log('Connected to the database');
+});
+
+//get all fundraisers(所有信息)
+app.get('/fundraisers', (req, res) => {
+	const query = `
+	SELECT FUNDRAISER.*, CATEGORY.NAME AS CATEGORY_NAME
+    FROM FUNDRAISER
+    JOIN CATEGORY ON FUNDRAISER.CATEGORY_ID = CATEGORY.CATEGORY_ID
+    WHERE FUNDRAISER.ACTIVE = true
+	`;
+
+	connection.query(query, (error, results) => {
+		if (error) {
+			res.status(500).send('Database query failed');
+		} else {
+			res.json(results);
+		}
+	});
+});
+
+//get categories(获取人物)
+app.get('/categories', (req, res) => {
+	const category = req.query.category;
+	const city = req.query.city;
+
+	let query = `
+	SELECT FUNDRAISER.*, CATEGORY.NAME AS CATEGORY_NAME 
+    FROM FUNDRAISER 
+    JOIN CATEGORY ON FUNDRAISER.CATEGORY_ID = CATEGORY.CATEGORY_ID 
+    WHERE FUNDRAISER.ACTIVE = true
+    `;
+
+    if (category) {
+    	query += `AND CATEGORY.NAME = '${category}'`;
+    }
+
+    if (city) {
+    	query += `AND FUNDRAISER.CITY = '${city}'`;
+    }
+
+    connection.query(query, (error, results) => {
+    	if (error) {
+    		res.status(500).send('Database query failed');
+    	} else {
+    		res.json(results);
+    	}
+    });
+});
+
+//GET fundraiser by ID（查询）
+app.get('/fundraiser/:id',(req, res) => {
+	const fundraiserID = req.params.id;
+	const query = `
+	SELECT FUNDRAISER.*, CATEGORY.NAME AS CATEGORY_NAME 
+    FROM FUNDRAISER 
+    JOIN CATEGORY ON FUNDRAISER.CATEGORY_ID = CATEGORY.CATEGORY_ID 
+    WHERE FUNDRAISER.FUNDRAISER_ID = ?
+    `;
+
+    connection.query(query, [fundraiserID], (error, results) => {
+    	if (error) {
+    		res.status(500).send('Database query failed');
+    	} else {
+    		res.json(results[0]);
+    	}
+    });
+});
+
+//server
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
